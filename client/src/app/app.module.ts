283959c49740b6f71a556181c934cb825f55534b
Dtaitt <donovan.taitt@yahoo.com>
2018-04-19 14:21:52 -0400

    created card display component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CardDisplayComponent } from './card-display/card-display.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CardDisplayComponent
   ],
   imports: [
     BrowserModule
